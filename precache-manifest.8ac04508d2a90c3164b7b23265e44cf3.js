4f015e0d16f887f61e7a94e7896e18a5d25fcec2
Daniel Lewis <dflewis816@gmail.com>
2020-10-15 12:40:33 -0500

    Updates

---
+++ precache-manifest.8ac04508d2a90c3164b7b23265e44cf3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10c473041a034f3bb253efdcdf236773",
+    "url": "/hololive-bingo/index.html"
+  },
+  {
+    "revision": "4afc8cad482ed83c0989",
+    "url": "/hololive-bingo/static/css/main.d27ae8f0.chunk.css"
+  },
+  {
+    "revision": "84067396deabfa7f2c01",
+    "url": "/hololive-bingo/static/js/2.da61a7db.chunk.js"
+  },
+  {
+    "revision": "4afc8cad482ed83c0989",
+    "url": "/hololive-bingo/static/js/main.459075c7.chunk.js"
+  },
+  {
+    "revision": "4c95e0be02241708774b",
+    "url": "/hololive-bingo/static/js/runtime-main.3ac53cb6.js"
+  }
+]);
